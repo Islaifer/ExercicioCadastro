4fb879fc5fbd810ca715573c83b06ab9b7201ae5
islai <islaifer2379@gmail.com>
2020-06-16 22:49:12 -0300

    Entrega do exercicio

---
+++ src/model/NO.java
@@ -0,0 +1,27 @@
+package model;
+
+import model.entity.Pessoa;
+
+public class NO {
+	private NO next;
+	private Pessoa data;
+	
+	/*-----Bob construtor------*/
+	public NO(Pessoa data) {
+		this.data = data;
+		this.next = null;
+	}
+	
+	public NO getNext() {
+		return next;
+	}
+	public void setNext(NO next) {
+		this.next = next;
+	}
+	public Pessoa getData() {
+		return data;
+	}
+	public void setData(Pessoa data) {
+		this.data = data;
+	}
+}
